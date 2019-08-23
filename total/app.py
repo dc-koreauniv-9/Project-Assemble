5b2466b66ea3586304b4173b00e816812ce5d14f
박영우 <yeongwu@Yp-MacBook-Pro.local>
2019-08-23 16:48:48 +0900

    db

--- total/app.py
+++ total/app.py
@@ -1,12 +1,17 @@
 from flask import Flask,render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///naver_news.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 @app.route('/1p/')
 def hello_world():
-    return render_template('1index.html')
+    table = Table2.query.filter_by(id=10)
+    return render_template('1index.html', table=table)
 
 @app.route('/2p/')
 def hello_world2():
@@ -19,6 +24,17 @@ def hello_world3():
 @app.route('/4p/')
 def hello_world4():
     return render_template('4tables.html')
+
+class Table2(db.Model):
+    __tablename__ = 'table2'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Text, nullable=False)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text
+                        , nullable=False)
+    link = db.Column(db.Text, nullable=False)
+
 
 
 
