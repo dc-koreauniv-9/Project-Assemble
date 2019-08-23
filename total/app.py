aefe9ee29ff1bd4158c1de89716a5b5f205c1dad
HuiYeon Yang <4kdmg3@naver.com>
2019-08-23 11:54:13 +0900

    soojung

--- total/app.py
+++ total/app.py
@@ -6,19 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/1p/')
 def hello_world():
-    return render_template('index1p.html')
+    return render_template('1index.html')
 
 @app.route('/2p/')
 def hello_world2():
-    return render_template('index2p.html')
+    return render_template('2introduce.html')
 
 @app.route('/3p/')
 def hello_world3():
-    return render_template('index3p.html')
+    return render_template('3charts.html')
 
 @app.route('/4p/')
 def hello_world4():
-    return render_template('index4p.html')
+    return render_template('4tables.html')
+
 
 
 if __name__ == '__main__':
