8dcd22d0eb61c28f4e9fe88de6d5567d8f30356d
HuiYeon Yang <4kdmg3@naver.com>
2019-08-26 18:06:02 +0900

    2P 검색기능 넣음

--- total/app.py
+++ total/app.py
@@ -22,7 +22,7 @@ def hello_world2():
     conn = sqlite3.connect('naver_news.db')
     conn.row_factory = sqlite3.Row
     c = conn.cursor()
-    c.execute("select * from table2 where id< 101")
+    c.execute("select * from table2 where id< 1001")
     rows = c.fetchall()
     return render_template('2_1news.html',rows = rows)
 
@@ -31,7 +31,7 @@ def hello_world3():
     conn = sqlite3.connect('TheMinJoo.db')
     conn.row_factory = sqlite3.Row
     c1 = conn.cursor()
-    c1.execute("select * from table1 where id<101")
+    c1.execute("select * from table1 where id<1001")
     rows = c1.fetchall()
     return render_template('2_2TheMinJoo.html',rows = rows)
 
@@ -40,7 +40,7 @@ def hello_world4():
     conn = sqlite3.connect('libertykorea0813.db')
     conn.row_factory = sqlite3.Row
     c2 = conn.cursor()
-    c2.execute("select * from table1 where id<101")
+    c2.execute("select * from table1 where id<1001")
     rows = c2.fetchall()
     return render_template('2_3LibertyKorea.html',rows = rows)
 
